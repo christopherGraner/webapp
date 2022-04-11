6b179fad4c7eaf06c600cbd71ccf6bdd696b526b
christopher graner <chrisgraner1@gmail.com>
2022-04-11 09:56:33 -0700

    loops

--- webapp.py
+++ webapp.py
@@ -2,13 +2,7 @@ from flask import Flask, url_for, render_template
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-# for State in data:
-#         options += Markup("<option value=\"" + s + "\">" + s + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
-#     return options
-#
-# for Year in data:
-#         options += Markup("<option value=\"" + y + "\">" + y + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
-#     return options
+
 
 @app.route("/")
 def render_main():
@@ -17,6 +11,14 @@ def render_main():
 @app.route("/p1")
 def render_page1():
     return render_template('page1.html')
+    
+for State in data:
+        options += Markup("<option value=\"" + s + "\">" + s + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
+        return options
+
+for Year in data:
+        options += Markup("<option value=\"" + y + "\">" + y + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
+        return options2
 
 @app.route("/p2")
 def render_page2():
